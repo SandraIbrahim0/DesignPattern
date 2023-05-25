f330d0df75c1fdbf62057fca70aa2e1de423d6a5
Sandra Ibrahim <Sandra.Ibrahim@geidea.net>
2023-05-25 17:16:32 +0300

    -liskov Principle

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DesignPattern.SOLID;
+using static DesignPattern.SOLID.LiskovPrinciple;
 using static DesignPattern.SOLID.OCP;
 
 /*Single Responsibility*/
@@ -15,19 +16,32 @@ using static DesignPattern.SOLID.OCP;
 
 /*OCP*/
 
-var apple = new Product("Apple", Color.Green, Size.Small);
-var tree = new Product("Tree", Color.Green, Size.Large);
-var house = new Product("House", Color.Blue, Size.Large);
+//var apple = new Product("Apple", Color.Green, Size.Small);
+//var tree = new Product("Tree", Color.Green, Size.Large);
+//var house = new Product("House", Color.Blue, Size.Large);
 
-Product[] products = { apple, tree, house };
+//Product[] products = { apple, tree, house };
 
-var betterFilter = new BetterFilter();
-Console.WriteLine("Green products :");
-foreach (var product in betterFilter.Filter(products, new ColorSpecification(Color.Green)))
-    Console.WriteLine($" - {product.Name} is green");
+//var betterFilter = new BetterFilter();
+//Console.WriteLine("Green products :");
+//foreach (var product in betterFilter.Filter(products, new ColorSpecification(Color.Green)))
+//    Console.WriteLine($" - {product.Name} is green");
 
-foreach (var product in betterFilter.Filter(products, new SizeSpecification(Size.Large)))
-    Console.WriteLine($" - {product.Name} is Large");
+//foreach (var product in betterFilter.Filter(products, new SizeSpecification(Size.Large)))
+//    Console.WriteLine($" - {product.Name} is Large");
 
-foreach (var product in betterFilter.Filter(products, new AddSpecification(Color.Blue, Size.Large)))
-    Console.WriteLine($" - {product.Name} is Large and Blue");
+//foreach (var product in betterFilter.Filter(products, new AddSpecification(Color.Blue, Size.Large)))
+//    Console.WriteLine($" - {product.Name} is Large and Blue");
+
+/*Liskov Principle*/
+
+static  int Area(Rectangle r) => r.Width * r.Height;
+
+Rectangle rc = new Rectangle(2, 3);
+Console.WriteLine($"{rc} has area {Area(rc)}");
+
+
+/*Square*/
+Rectangle sq = new Square();
+sq.Width = 4;
+Console.WriteLine($"{sq} has area {Area(sq)}");
